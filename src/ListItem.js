0231fe21f9dacb855766250255dd93d4a4cdab52
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-10-18 21:15:34 +0200

    setLevelClassName function

--- src/ListItem.js
+++ src/ListItem.js
@@ -10,23 +10,35 @@ function formattedPrice(currencyCode, price) {
   return `${price} ${currencyCode}`;
 }
 
+function setLevelClassName(quantity) {
+  if (quantity <= 10) {
+    return 'low';
+  }
+
+  if (quantity <= 20) {
+    return 'medium';
+  }
+
+  if (quantity > 20) {
+    return 'high';
+  }
+}
+
 function ListItem( { item }) {
   return (
     <div className="item">
       <div className="item-image">
         <a href={ item.url }>
-          <img src={ item.hasOwnProperty('MainImage') && item.MainImage.url_570xN } alt={item.listing_id}></img>
+          <img src={ item.hasOwnProperty('MainImage') && item.MainImage.url_570xN } alt={ item.listing_id }></img>
         </a>
       </div>
       <div className="item-details">
-        <p className="item-title">{ item && item.title.length <= 50 ? item.title : item.title.slice(0, 50) + '...'}</p> 
+        <p className="item-title">{ item.title.length <= 50 ? item.title : item.title.slice(0, 50) + '...'}</p> 
         <p className="item-price">{ formattedPrice(item.currency_code, item.price) }</p>
-        <p className="item-quantity level-medium">12 left</p>
+        <p className={ `item-quantity level-${setLevelClassName(item.quantity)}` }>{ item.quantity } left</p>
       </div>
     </div>
   )
 }
 
 export default ListItem;
-
-// <= 50 ? item.title : item.title.slice(0, 51) + '...'
